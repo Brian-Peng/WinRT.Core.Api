ab1ceedf0d2b343286d52f0d4c25b633d16f5885
2411725170 <py16680102604@163.com>
2020-12-18 14:35:40 +0800

    删除多余的代码

--- WinRT.Core.Api/Extensions/AutofacModuleRegister.cs
+++ WinRT.Core.Api/Extensions/AutofacModuleRegister.cs
@@ -38,21 +38,10 @@ namespace WinRT.Core.Api.Extensions
 
             // AOP 开关，如果想要打开指定的功能，只需要在 appsettigns.json 对应对应 true 就行。
             var cacheType = new List<Type>();
-            if (Appsettings.app(new string[] { "AppSettings", "RedisCachingAOP", "Enabled" }).ObjToBool()) // 按照 appsettigns.json中的层级的顺序，依次写出来
-
-            {
-                //builder.RegisterType<BlogRedisCacheAOP>();
-                //cacheType.Add(typeof(BlogRedisCacheAOP));
-            }
-            if (Appsettings.app(new string[] { "AppSettings", "MemoryCachingAOP", "Enabled" }).ObjToBool())
+            if (Appsettings.app(new string[] { "AppSettings", "MemoryCachingAOP", "Enabled" }).ObjToBool()) // 按照 appsettigns.json中的层级的顺序，依次写出来
             {
                 builder.RegisterType<CacheAOP>();
                 cacheType.Add(typeof(CacheAOP));
-            }
-            if (Appsettings.app(new string[] { "AppSettings", "TranAOP", "Enabled" }).ObjToBool())
-            {
-                //builder.RegisterType<BlogTranAOP>();
-                //cacheType.Add(typeof(BlogTranAOP));
             }
             if (Appsettings.app(new string[] { "AppSettings", "LogAOP", "Enabled" }).ObjToBool())
             {
